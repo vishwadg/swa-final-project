35293507d8a8876a44c4e477c4d31aa8f546bb3a
pradip <pradipdhungel12@gmail.com>
2022-10-19 15:15:13 -0500

    all testing of AuthenticationServiceImpl test

---
+++ commons-module/src/main/java/com/example/commonsmodule/CommonsModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.commonsmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonsModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonsModuleApplication.class, args);
+    }
+
+}
