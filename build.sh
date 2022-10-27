bdd34d6baa361009c96ceddcc800ea34bf984b38
vishwa <vishwadeep.ghimire@gmail.com>
2022-10-27 01:05:55 -0500

    Modification

--- build.sh
+++ build.sh
@@ -6,5 +6,6 @@ folders=(
 
 for folder in ${folders[@]}
  do
-    docker build . --build-arg build_folder=${folder} -t "vishwaghimire/${folder}:latest"
+#    docker build . --build-arg build_folder=${folder} -t "vishwaghimire/${folder}:latest"
+     docker build -t "vishwaghimire/${folder}:latest" --build-arg build_folder=${folder} .
 done
